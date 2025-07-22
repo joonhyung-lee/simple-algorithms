b602be9103e49b83884c9f03d8d4c1a40693b26e
joonhyung-lee <dlwnsgud8823@korea.ac.kr>
2025-07-22 14:47:34 +0900

    feat: add `tree`

---
+++ algorithms/data_structure/tree_traversal.py
@@ -0,0 +1,68 @@
+"""
+python tree_traversal.py
+
+# Input
+7
+A B C
+B D E
+C F G
+D None None
+E None None
+F None None
+G None None
+
+# Output
+A B D E C F G 
+D B E A F C G 
+D E B F G C A 
+
+"""
+# Node Class
+class Node:
+    def __init__(self, data, left_node, right_node):
+        self.data = data
+        self.left_node = left_node
+        self.right_node = right_node
+
+# Preorder Traversal
+def pre_order(node):
+    print(node.data, end=' ')
+    if node.left_node != None:
+        pre_order(tree[node.left_node])
+    if node.right_node != None:
+        pre_order(tree[node.right_node])
+
+# Inorder Traversal
+def in_order(node):
+    if node.left_node != None:
+        in_order(tree[node.left_node])
+    print(node.data, end=' ')
+    if node.right_node != None:
+        in_order(tree[node.right_node])
+
+# Postorder Traversal
+def post_order(node):
+    if node.left_node != None:
+        post_order(tree[node.left_node])
+    if node.right_node != None:
+        post_order(tree[node.right_node])
+    print(node.data, end=' ')
+    
+
+### Test Case
+n = int(input())
+tree = {}
+
+for i in range(n):
+    data, left_node, right_node = input().split()
+    if left_node == "None":
+        left_node = None
+    if right_node == "None":
+        right_node = None
+    tree[data] = Node(data, left_node, right_node)
+
+pre_order(tree['A'])
+print()
+in_order(tree['A'])
+print()
+post_order(tree['A'])
